a8e94bdad3d21a77e8b97555093262fb3817ac0c
cmgboji <157683322+cmgboji@users.noreply.github.com>
2025-12-16 14:54:10 -0600

    Data storage implemented

--- syllabus script.py
+++ syllabus script.py
@@ -2,27 +2,65 @@ import requests
 from bs4 import BeautifulSoup
 import io
 from pypdf import PdfReader
+import pandas as pd
+
+raw_data = []
 
 base_url = 'https://utdirect.utexas.edu'
-url = "https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/?year=&semester=&department=A+I&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In+Residence&search=Search"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+url_list = ['https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/?year=&semester=&department=A+I&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In+Residence&search=Search',
+            'https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/?year=&semester=&department=AED&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In+Residence&search=Search']
+#url = "https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/?year=&semester=&department=A+I&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In+Residence&search=Search"
 
-syllabi = []
-for link in soup.find_all('a', href=True):
-    href = link.get('href')
-    if 'download' in href:
-        href = base_url + href
-        syllabi.append(href)
+for url in url_list:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-syllabi = syllabi[1::2]
+    header_row = soup.find('tr', class_='tbh header')
+    table = header_row.find_parent('table')
+    for row in table.find_all('tr')[1:]:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        raw_data.append(cols)
+    
+
+data = {'Semester': [], 'Department': [], 'Course Number': [], 'Title': [], 
+        'Unique': [], 'Instructor': [], 'Syllabus': [], 'Survey': []}
+
+data['Semester'] = [row[0] for row in raw_data]
+data['Department'] = [row[1] for row in raw_data]
+data['Course Number'] = [row[2] for row in raw_data]
+data['Title'] = [row[3] for row in raw_data]
+data['Unique'] = [row[4] for row in raw_data]
+data['Instructor'] = [row[5] for row in raw_data]
+data['Syllabus'] = [row[6] for row in raw_data]
+data['Survey'] = [row[7] for row in raw_data]
 
 text = []
-for link in syllabi:
-    response = requests.get(link)
-    with io.BytesIO(response.content) as f:
-        reader = PdfReader(f)
-        for page in reader.pages:
-            text.append(page.extract_text())
 
-print(text)
+for url in url_list:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    syllabi = []
+    for link in soup.find_all('a', href=True):
+        href = link.get('href')
+        if 'download' in href:
+            href = base_url + href
+            syllabi.append(href)
+
+    syllabi = syllabi[1::2]
+
+    for link in syllabi:
+        response = requests.get(link)
+        with io.BytesIO(response.content) as f:
+            reader = PdfReader(f)
+            full_text = ""
+            for page in reader.pages:
+                page_text = page.extract_text()
+                full_text += page_text + "\n"
+            text.append(full_text)
+
+
+data['Syllabus'] = text
+df = pd.DataFrame(data)
+print(df)
