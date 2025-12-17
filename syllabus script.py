55554f9f935d8dd2fb25b91951401e26a96c013d
cmgboji <157683322+cmgboji@users.noreply.github.com>
2025-12-17 12:56:52 -0600

    Initial syllabi cleaning

--- syllabus script.py
+++ syllabus script.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import io
+import logging
+logging.getLogger("pypdf").setLevel(logging.ERROR) # silencing pypdf warnings
 from pypdf import PdfReader
+import re
+import nltk
+from nltk.corpus import stopwords
 import pandas as pd
 
 raw_data = []
+text = []
+stop_words = set(stopwords.words('english'))
 
 base_url = 'https://utdirect.utexas.edu'
 url_list = ['https://utdirect.utexas.edu/apps/student/coursedocs/nlogon/?year=&semester=&department=A+I&course_number=&course_title=&unique=&instructor_first=&instructor_last=&course_type=In+Residence&search=Search',
@@ -15,40 +22,22 @@ for url in url_list:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
+    # Constructing data table from extracted HTML
     header_row = soup.find('tr', class_='tbh header')
     table = header_row.find_parent('table')
     for row in table.find_all('tr')[1:]:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
         raw_data.append(cols)
-    
-
-data = {'Semester': [], 'Department': [], 'Course Number': [], 'Title': [], 
-        'Unique': [], 'Instructor': [], 'Syllabus': [], 'Survey': []}
-
-data['Semester'] = [row[0] for row in raw_data]
-data['Department'] = [row[1] for row in raw_data]
-data['Course Number'] = [row[2] for row in raw_data]
-data['Title'] = [row[3] for row in raw_data]
-data['Unique'] = [row[4] for row in raw_data]
-data['Instructor'] = [row[5] for row in raw_data]
-data['Syllabus'] = [row[6] for row in raw_data]
-data['Survey'] = [row[7] for row in raw_data]
-
-text = []
-
-for url in url_list:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
 
     syllabi = []
     for link in soup.find_all('a', href=True):
         href = link.get('href')
         if 'download' in href:
-            href = base_url + href
+            href = base_url + href # full URL of syllabus PDF
             syllabi.append(href)
 
-    syllabi = syllabi[1::2]
+    syllabi = syllabi[1::2] # filtering for syllabi and removing cover letters
 
     for link in syllabi:
         response = requests.get(link)
@@ -56,11 +45,22 @@ for url in url_list:
             reader = PdfReader(f)
             full_text = ""
             for page in reader.pages:
-                page_text = page.extract_text()
+                page_text = page.extract_text().strip()
                 full_text += page_text + "\n"
             text.append(full_text)
+    
+def clean_text(text):
+    text = re.sub(r'[^a-z\s]', '', text.lower())
+    tokens = [word for word in text.split() if word not in stop_words]
+    cleaned_text = ' '.join(tokens)
+    return cleaned_text
 
+text = [clean_text(t) for t in text]
 
+data = pd.DataFrame(raw_data, columns = ['Semester', 'Department', 'Course Number', 
+                                         'Title', 'Unique', 'Instructor', 'Syllabus', 
+                                         'Survey'])
 data['Syllabus'] = text
-df = pd.DataFrame(data)
-print(df)
+data['Course Code'] = data['Department'] + ' ' + data['Course Number']
+data.drop(columns=['Unique', 'Survey'], inplace=True)
+print(data)
